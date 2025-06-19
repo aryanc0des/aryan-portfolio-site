fa1a7168ba578b5bba28f32604371ecf94e19d98
aryanc0des <134934556+aryanc0des@users.noreply.github.com>
2025-06-19 23:21:29 +0530

    added the form submit backend api

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ const sections = document.querySelectorAll('.fade-in-section');
   sections.forEach(section => {
     observer.observe(section);
   });
+
+  const form = document.getElementById("contact-form");
+
+  form.addEventListener("submit", function(event){
+    event.preventDefault(); // stops default refresh
+    console.log("Form Submitted");
+
+    emailjs.sendForm("service_r7lnalj", "template_mxf0joh", form, "g2hI2Uiq77uyzdN_Q")
+        .then(function(response){
+            console.log("SUCCESSFULL", response.status, response.text);
+            alert("Message sent successfully ✅");
+            form.reset();
+        })
+        .catch(function(error){
+            console.error("FAILED");
+            alert("❌Failed to send the message, please try again later");
+        })
+  });;
